a6d1a0e7da3ad2da82ff4eb7b0489fe46302455f
Xiao Yu <me@xyu.io>
2014-12-09 19:28:17 +0000

    Add a classmap and an autoloader

---
+++ src/autoload.php
@@ -0,0 +1,8 @@
+<?php
+// Autoload from classmap
+spl_autoload_register( function ( $class ) {
+  $classmap = require __DIR__ . '/autoload_classmap.php';
+  if( array_key_exists( $class, $classmap ) ) {
+    require $classmap[ $class ];
+  }
+} );
