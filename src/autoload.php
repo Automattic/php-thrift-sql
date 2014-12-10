75bbee10c86c11813fee59e4580398fa3bc590dc
Xiao Yu <me@xyu.io>
2014-12-10 17:04:26 +0000

    We only need to include class defs once

--- src/autoload.php
+++ src/autoload.php
@@ -3,6 +3,6 @@
 spl_autoload_register( function ( $class ) {
   $classmap = require __DIR__ . '/autoload_classmap.php';
   if( array_key_exists( $class, $classmap ) ) {
-    require $classmap[ $class ];
+    require_once $classmap[ $class ];
   }
 } );
