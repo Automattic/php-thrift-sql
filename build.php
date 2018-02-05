f2efa33cc673219923256477965281338576ed24
Xiao Yu <me@xyu.io>
2018-02-05 14:54:59 +0000

    Check that we can write phar in build script

--- build.php
+++ build.php
@@ -2,7 +2,15 @@
 
   // Check we have dev tools
   if ( !file_exists( __DIR__ . '/vendor/bin/php-generate-autoload' ) ) {
-    echo "Please install dev tools with:\n\n$ composer install\n";
+    echo "Please install dev tools with:\n";
+    echo "$ composer install\n";
+    exit(1);
+  }
+
+  // Check we can write phar
+  if ( !Phar::canWrite() ) {
+    echo "Can not write phar please run build script with:\n";
+    echo "$ php -d phar.readonly=0 {$argv[0]}\n";
     exit(1);
   }
 
